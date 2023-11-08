5c41a599104c568e1f7db213a9ff8b2ef5a452ff
Rafael Damasco <rafaeldamasco_@hotmail.com>
2023-11-08 15:35:55 -0300

    feat: add category and categories components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import Image from "next/image";
+import Categories from "./components/categories";
 
 export default function Home() {
   return (
     <div className="p-5">
       <Image
         src="/banner-home-01.png"
-        className="w-ful h-auto"
+        className="h-auto w-full"
         height={0}
         width={0}
         sizes="100vw"
         alt="Ate 55% de desconto esse mes"
       />
+      <div className="mt-8">
+        <Categories />
+      </div>
     </div>
   );
 }
