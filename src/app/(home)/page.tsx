87ca34bebc740d6c14889a40b18cf9f5bea77deb
Rafael Damasco <rafaeldamasco_@hotmail.com>
2023-11-13 14:36:11 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -26,8 +26,17 @@ export default async function Home() {
         <Categories />
       </div>
       <div className="mt-8">
+        <p className="mb-3 pl-5 font-semibold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+      <Image
+        src="/banner-home-02.png"
+        className="h-auto w-full px-5"
+        height={0}
+        width={0}
+        sizes="100vw"
+        alt="Ate 55% de desconto em mouses"
+      />
     </div>
   );
 }
