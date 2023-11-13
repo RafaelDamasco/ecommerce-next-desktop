b702d87b52be3bfc53cbe90ed60022360db55748
Rafael Damasco <rafaeldamasco_@hotmail.com>
2023-11-13 15:38:16 -0300

    feat: add footer component to home page

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-center bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+      2023 Copyright <span className="font-semibold">Ecommerce</span>
+    </div>
+  );
+};
+
+export default Footer;
