3417e67ae07ad601500d34b31c6ee09cd3429aaa
Rafael Damasco <rafaeldamasco_@hotmail.com>
2024-02-02 11:23:33 -0300

    feat: cart products can be scrolled and keeps the order summary visible

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -5,11 +5,13 @@ import { CartContext } from "@/provider/cart";
 import CartItem from "./cart-item";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
+import { Button } from "./button";
+import { ScrollArea } from "./scroll-area";
 
 const Cart = () => {
   const { products, total, subtotal, totalDiscount } = useContext(CartContext);
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant={"outline"}
@@ -17,18 +19,24 @@ const Cart = () => {
         <ShoppingCartIcon size={16} />
         Cart
       </Badge>
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <CartItem
-              product={computeProductTotalPrice(product) as any}
-              key={product.id}
-            />
-          ))
-        ) : (
-          <p className="text-center font-semibold">Empty cart.</p>
-        )}
+
+      <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-5">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem
+                  product={computeProductTotalPrice(product) as any}
+                  key={product.id}
+                />
+              ))
+            ) : (
+              <p className="text-center font-semibold">Empty cart.</p>
+            )}
+          </div>
+        </ScrollArea>
       </div>
+
       <div className="flex flex-col gap-3">
         <Separator />
 
@@ -57,6 +65,8 @@ const Cart = () => {
           <p>Total</p>
           <p>- R${total.toFixed(2)}</p>
         </div>
+
+        <Button className="mt-7 font-bold uppercase">Checkout</Button>
       </div>
     </div>
   );
